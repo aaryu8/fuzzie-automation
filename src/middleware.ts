3d5216621bd7b8d4d96e243508bb46e332d250d5
Aryan Kaushik <167664714+aaryu8@users.noreply.github.com>
2025-11-05 12:05:18 +0530

    Updated middleware.ts-3

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,23 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { authMiddleware } from '@clerk/nextjs'
 
-const isPublicRoute = createRouteMatcher([
-  '/',
-  '/sign-in(.*)',
-  '/sign-up(.*)',
-  '/api/clerk-webhook',
-  '/api/drive-activity/notification',
-  '/api/payment/success',
-])
-
-const isIgnoredRoute = createRouteMatcher([
-  '/api/auth/callback/discord',
-  '/api/auth/callback/notion',
-  '/api/auth/callback/slack',
-  '/api/flow',
-  '/api/cron/wait',
-])
-
-export default clerkMiddleware((auth, request) => {
-  if (isIgnoredRoute(request)) return
-  if (!isPublicRoute(request)) auth().protect()
+export default authMiddleware({
+  publicRoutes: [
+    '/',
+    '/sign-in(.*)',
+    '/sign-up(.*)',
+    '/api/clerk-webhook',
+    '/api/drive-activity/notification',
+    '/api/payment/success',
+  ],
+  ignoredRoutes: [
+    '/api/auth/callback/discord',
+    '/api/auth/callback/notion',
+    '/api/auth/callback/slack',
+    '/api/flow',
+    '/api/cron/wait',
+  ],
 })
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 }
