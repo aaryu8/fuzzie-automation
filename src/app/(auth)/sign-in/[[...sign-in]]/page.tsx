adefcafdeee142c682980f223cab72d7f74390fa
Aryan Kaushik <aryan8kaushik@google.com>
2025-11-05 05:05:24 +0000

    Fix : updated sign and sign in pages to stop loop by adding redirect url and after auth url

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignIn />
+  return <SignIn afterSignInUrl={process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL} redirectUrl={process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL}/>
 }
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignUp />
+  return <SignUp afterSignInUrl={process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL} redirectUrl={process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL}/>
 }
